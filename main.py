d4654ba2daaa19f7207d7dfa746dc0f9776e9d03
adultmalehuman <adultmalehuman@gmail.com>
2022-06-16 12:28:43 -0300

    grid layout and widget padding

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 # import tkinter
 from tkinter import *
 
-# REFERENCE: https://docs.python.org/3/library/tkinter.html
+# REFERENCES:
+#  * https://docs.python.org/3/library/tkinter.html
+#  * https://replit.com/@appbrewery/tkinter-widget-demo#main.py
 window = Tk()
 window.title("My First GUI Program!")
 window.minsize(width=500, height=300)
+window.config(padx=30, pady=30)
 
 # label
 my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
-my_label.pack()
+# my_label.pack()
 # my_label.pack(side="bottom")
 # my_label.pack(side="left")
+# my_label.place(x=100, y=0)
+my_label.grid(column=0, row=0)
 # two ways to do the same thing...
 my_label["text"] = "My New Text"
 my_label.config(text="My Even Newer Text")
+my_label.config(padx=50, pady=50)
 
 
 # button
@@ -23,15 +29,17 @@ def button_clicked():
     my_label.config(text=input.get())
 
 
-button = Button(text="Click Me!", command=button_clicked)
-button.pack()
+button_a = Button(text="Click Me!", command=button_clicked)
+button_a.grid(column=1, row=1)
+
+button_b = Button(text="also click me", command=button_clicked)
+button_b.grid(column=2, row=0)
 
 
-# entry (text input)
-
+# entry (single line text input)
 input = Entry(width=10)
-input.pack()
-print(input.get())
+# input.pack()
+input.grid(column=3, row=2)
 
 
 window.mainloop()
